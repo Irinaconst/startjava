86290ab0538711c59c2135d70a725429b4c3fbcb
Irina Pelevinova <ireneconst92@mail.ru>
2020-04-25 11:52:08 +0700

    implement HW_1

--- Lesson_1/Variable.java
+++ Lesson_1/Variable.java
@@ -1,28 +1,25 @@
 public class Variable {
 	
 	public static void main(String[] args) {
-		
 		//Ноутбук ASUS F507UF;
-		char windows = 10;
-		int w = windows - 0;
-		short vuosi = 2018;
+		byte windows = 10;
+		short vuosi = 2018; //vuosi - год;
 		boolean gaming = false;
 		float diagonal = 15;
-		byte cores = 2;
+		char cores = '2';
 		int ramGB = 4;
 		int hddGB = 1000;
 		long widthMm = 365;
 		double thicknessMm = 21.9;
 		
-		System.out.println("Windows"+w); 
-		System.out.println("Year of manufacture:"+vuosi);
-		System.out.println("Gaming notebook:"+gaming);
-		System.out.println("diagonal:"+diagonal);
-		System.out.println("number of processor cores:"+cores);
-		System.out.println("RAM:"+ramGB);
-		System.out.println("HDD:"+hddGB);
-		System.out.println("Widh:"+widthMm);
-		System.out.println("Thickness:"+thicknessMm);
-	
+		System.out.println("Windows" + windows); 
+		System.out.println("Year of manufacture:" + vuosi);
+		System.out.println("Gaming notebook:" + gaming);
+		System.out.println("diagonal:" + diagonal);
+		System.out.println("number of processor cores:" + cores);
+		System.out.println("RAM:" + ramGB);
+		System.out.println("HDD:" + hddGB);
+		System.out.println("Widh:" + widthMm);
+		System.out.println("Thickness:" + thicknessMm);
 	}
 }
