039e37c92c17c8e331121803fda215cc5187db3f
Irina Pelevinova <ireneconst92@mail.ru>
2020-04-30 20:36:41 +0700

    implement HW_5

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -4,19 +4,39 @@ public class Calculator {
 		double a = 3;
 		double b = 2;
 		double c;
-		c = a / b;
-		if (c == a * b) {
-			System.out.println(c);
-		} else if (c == a - b) {
-			System.out.println(c);
-		} else if (c == a / b) {
-			System.out.println(c);
-		} else if (c == a % b) {
-			System.out.println(c);
-		} else if (c == a * b) {
-			System.out.println(c);
-		} else if (c == a * a ) {
-			System.out.println(c);
-		}
+		double addition = '+';
+    	double subtraction = '-';
+    	double multiplication = '*';
+    	double division = '/';
+    	double modulus = '%';
+    	double exponentiation = '^';
+    	double operation = division;
+    	if (operation == addition) {
+    		c = a + b; 
+    		System.out.println(a + "+" + b + "=" + c);
+    	}
+    	else if (operation == subtraction) {
+    		c = a - b;
+    		System.out.println(a + "-" + b + "=" + c);
+    	}
+    	else if (operation == multiplication) {
+    		c = a * b;
+    		System.out.println(a + "*" + b + "=" + c);
+    	}
+    	else if (operation == division) {
+    		c = a / b;
+    		System.out.println(a + "/" + b + "=" + c);
+    	}
+    	else if (operation == modulus) {
+    		c = a % b;
+    		System.out.println(a + "%" + b + "=" + c);
+    	}
+    	else if (operation == exponentiation) {
+    		 c = 1;
+    		for (int i = 0; i < b; i++) {
+    			c *= a;
+    		}
+    		System.out.println(a + "^" + b + "=" + c);
+    	}
 	}
 }
--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -1,16 +1,24 @@
 public class MyFirstGame {
 
 	public static void main(String[] args) {
-		int rand = 45; //компьютер загадывает целое число
-		int num = 45; //число, введенное пользователем
-		do {
-			if (num > rand) {
-				System.out.println("Введенное вами число больше того, что загадал компьютер!");
-			} else if (num < rand) {
-				System.out.println("Введенное вами число меньше того, что загадал компьютер!");	
-			} else {
-			System.out.println("Вы угадали!");
+		int numberToGuess = 33;
+		int guess;
+		boolean victory = false;
+		while ( victory == false) {
+			guess = 15; 
+			if (guess == numberToGuess) {
+				victory = true;
 			}
-		}while (num != rand);	
+			else if (guess < numberToGuess) {
+				System.out.println("Введенное вами число меньше того, что загадал компьютер!");	
+			}
+			else if (guess > numberToGuess) {
+				System.out.println("Введенное вами число больше того, что загадал компьютер!");
+			} victory = true; 
+			} 
+			System.out.println("Вы угадали!");
+		}
 	}
-}
+
+
+		
