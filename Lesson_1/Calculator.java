c997e87385fc9c3abc8849dbdebb8d9440428e8a
Irina Pelevinova <ireneconst92@mail.ru>
2020-05-02 18:10:17 +0700

    implement HW_5

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -3,34 +3,30 @@ public class Calculator {
 	public static void main(String[] args) {
 		double a = 3;
 		double b = 2;
-		double c;
+		double rezult;
 		double sign = '^';
-    		if (sign == '+') {
-    			c = a + b; 
-    			System.out.println(a + "+" + b + "=" + c);
-    		}
-    		else if (sign == '-') {
-    			c = a - b;
-    			System.out.println(a + "-" + b + "=" + c);
-    		}
-    		else if (sign == '*') {
-    			c = a * b;
-    			System.out.println(a + "*" + b + "=" + c);
-    		}
-    		else if (sign == '/') {
-    			c = a / b;
-    			System.out.println(a + "/" + b + "=" + c);
-    		}
-    		else if (sign == '%') {
-    			c = a % b;
-    			System.out.println(a + "%" + b + "=" + c);
-    		}
-    		else if (sign == '^') {
-    			 c = 1;
-    			for (int i = 0; i < b; i++) {
-    			c *= a;
-    		}
-    			System.out.println(a + "^" + b + "=" + c);
+
+    	if (sign == '+') {
+    		rezult = a + b; 
+    		System.out.println(a + "+" + b + "=" + rezult);
+    	} else if (sign == '-') {
+    		rezult = a - b;
+    		System.out.println(a + "-" + b + "=" + rezult);
+    	} else if (sign == '*') {
+    		rezult = a * b;
+    		System.out.println(a + "*" + b + "=" + rezult);
+    	} else if (sign == '/') {
+    		rezult = a / b;
+    		System.out.println(a + "/" + b + "=" + rezult);
+    	} else if (sign == '%') {
+    		rezult = a % b;
+    		System.out.println(a + "%" + b + "=" + rezult);
+    	}else if (sign == '^') {
+    		rezult = 1;
+    		for (int i = 0; i < b; i++) {
+    		rezult *= a;
+    	}
+    		System.out.println(a + "^" + b + "=" + rezult);
     	}
 	}
 }
--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -2,18 +2,20 @@ public class MyFirstGame {
 
 	public static void main(String[] args) { 
 		int numberToGuess = 33;
-		int guess = (int) (Math.random() * 100);
+		int guess = (int) (Math.random() * 101);
 		boolean victory = false;
-			while (victory == false) {
-				if (guess == numberToGuess) {
-					victory = true;
-				} else if (guess < numberToGuess) {
-					System.out.println("Введенное вами число меньше того, что загадал компьютер!");	
-				} else if (guess > numberToGuess) {
-					System.out.println("Введенное вами число больше того, что загадал компьютер!");
-				} victory = true  ; 
-			}
-				System.out.println("Вы угадали!");
+
+		while (victory == false) {
+			if (guess == numberToGuess) {
+				victory = true;
+			} else if (guess < numberToGuess) {
+				System.out.println("Введенное вами число меньше того, что загадал компьютер!");	
+			} else if (guess > numberToGuess) {
+				System.out.println("Введенное вами число больше того, что загадал компьютер!");
+			} victory = true  ; 
+		}
+
+		System.out.println("Вы угадали!");
 	}
 }
 
